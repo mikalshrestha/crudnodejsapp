e2c6515bc75b26e32e3456bec5ddb7586db9f08c
Mikal Shrestha <mikalshrestha@Mikals-MacBook-Pro.local>
2021-06-20 07:59:21 +0545

    crud nodejs app

---
+++ models/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+ 
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('post', PostSchema);
